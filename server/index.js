aeedff33278dace4edf429816e23651c24848b0b
gourav <kartikpal1402@gmail.com>
2025-12-18 22:12:44 +0530

    Add: database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db.js";
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
