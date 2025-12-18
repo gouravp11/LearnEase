470bb6b7b6a03c6e779e17defe3fd3678f84bb8a
gourav <kartikpal1402@gmail.com>
2025-12-18 22:59:39 +0530

    Add: authentication routes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
+import authRoutes from "./routes/auth.js";
 dotenv.config();
 
+// Connect to database
 connectDB();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log("server is running at PORT", PORT);
+    console.log("Server is running on PORT", PORT);
 });
