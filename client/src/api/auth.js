6763989728438a30e8a6c22817372ad4d276cbb3
gourav <kartikpal1402@gmail.com>
2025-12-19 19:15:31 +0530

    Client Add: api wrappers

---
+++ client/src/api/auth.js
@@ -0,0 +1,5 @@
+import api from "./axiosInstance";
+
+export const signup = (data) => api.post("/auth/signup", data);
+export const signin = (data) => api.post("/auth/signin", data);
+export const logout = () => api.post("/auth/logout");
